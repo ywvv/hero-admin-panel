05ed200e2c6dc161531f21845214427290d87182
ywvv <ywvvvw@gmail.com>
2023-01-27 15:17:22 +0200

    feat: add active filter

--- src/components/heroesFilters/HeroesFilters.jsx
+++ src/components/heroesFilters/HeroesFilters.jsx
@@ -1,15 +1,61 @@
+import { useHttp } from "../../hooks/http.hook";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  filtersFetching,
+  filtersFetched,
+  filtersFetchingError,
+  activeFilterChanged,
+} from "../../actions";
+import Spinner from "../spinner/Spinner";
+
 const HeroesFilters = () => {
+  const { filters, filtersLoadingStatus, activeFilter } = useSelector(
+    (state) => state
+  );
+  const dispatch = useDispatch();
+  const { request } = useHttp();
+
+  useEffect(() => {
+    dispatch(filtersFetching());
+    request("http://localhost:3001/filters")
+      .then((data) => dispatch(filtersFetched(data)))
+      .catch(() => dispatch(filtersFetchingError()));
+  }, []);
+
+  if (filtersLoadingStatus === "loading") {
+    return <Spinner />;
+  } else if (filtersLoadingStatus === "error") {
+    return <h5 className="text-center mt-5">Loading error</h5>;
+  }
+
+  const renderFilters = (arr) => {
+    if (arr.length === 0) {
+      return <h5 className="text-center mt-5">Фильтры не найдены</h5>;
+    }
+
+    return arr.map(({ name, className, label }) => {
+      return (
+        <button
+          key={name}
+          id={name}
+          className={className + (name === activeFilter ? " active" : null)}
+          onClick={() => dispatch(activeFilterChanged(name))}
+        >
+          {label}
+        </button>
+      );
+    });
+  };
+
+  const elements = renderFilters(filters);
+
   return (
     <div className="card shadow-lg mt-4">
       <div className="card-body">
         <p className="card-text">Filter heroes by elements</p>
-        <div className="btn-group">
-          <button className="btn btn-outline-dark active">All</button>
-          <button className="btn btn-danger">Fire</button>
-          <button className="btn btn-primary">Water</button>
-          <button className="btn btn-success">Wind</button>
-          <button className="btn btn-secondary">Earth</button>
-        </div>
+        <div className="btn-group">{elements}</div>
       </div>
     </div>
   );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,8 @@ const initialState = {
   heroes: [],
   heroesLoadingStatus: "idle",
   filters: [],
+  filtersLoadingStatus: "idle",
+  activeFilter: "all",
 };
 
 const reducer = (state = initialState, action) => {
@@ -31,6 +33,27 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         heroes: [...state.heroes, action.payload],
+      };
+    case "FILTERS_FETCHING":
+      return {
+        ...state,
+        filtersLoadingStatus: "loading",
+      };
+    case "FILTERS_FETCHED":
+      return {
+        ...state,
+        filters: action.payload,
+        filtersLoadingStatus: "idle",
+      };
+    case "FILTERS_FETCHING_ERROR":
+      return {
+        ...state,
+        filtersLoadingStatus: "error",
+      };
+    case "ACTIVE_FILTER_CHANGED":
+      return {
+        ...state,
+        activeFilter: action.payload,
       };
     default:
       return state;
